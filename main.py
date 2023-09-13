6266d4443c929e4fb250af00eb5ed5e7de01940d
bl4ckD0g0 <alexheavy1@gmail.com>
2023-09-13 22:42:02 +0200

    Añadidas "CANDIDATURAS"

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-
 import os
 import os.path as path
 from pathlib import Path
+from DatosCandidaturas import CANDIDATURAS
 
 import fitz
 
@@ -35,38 +35,36 @@ def procesar_provincia(doc_name):
     os.mkdir(nombre_provincia)
     os.chdir(nombre_provincia)
 
+    # PROCESAMOS LA PRIMERA POR SEPARADO
+    lines = lines[2:]
+    procesar_municipio(lines)
 
+    # Después de generar el 1er Municipio, eliminas la 1a pagina y procesas el resto
+    doc.delete_page(0)
 
-
-
-
-
-
-    # out = open(Path(OUTPUT_FOLDER).joinpath(nombre_municipio), "wb")
-    # out.write('Municipio; Número de votos; Porcentaje\n'.encode('utf8'))
-    #
-    # lines = lines[2:]
-    # procesar_municipio(lines, out)
-    #
-    # doc.delete_page(0)
-    #
-    # for page_number, page in enumerate(doc):
-    #     text = page.get_text()
-    #     lines = text.split('\n')
-    #     procesar_municipio(lines, out)
-    # out.close()
+    for page_number, page in enumerate(doc):
+        text = page.get_text()
+        lines = text.split('\n')
+        procesar_municipio(lines)
 
     os.chdir(current_folder)
 
 
-def procesar_municipio(lines, out):
-    municipio = lines[1]
-    numero_votos = porcentaje = 0
+def procesar_municipio(lines):
+    municipio = lines[1].replace("/", "_")
+    out = open(municipio, "wb")
+    out.write('Partido; Número de votos; Porcentaje\n'.encode('utf8'))
+    ultimo_partido_leido = ''
+    votos = porcentaje = 0
     for j, line in enumerate(lines):
-            numero_votos = lines[j + 1]
+        partido = lines[j]
+        if partido in CANDIDATURAS and partido != ultimo_partido_leido:
+            votos = lines[j + 1]
             porcentaje = lines[j + 2]
-            break
-    out.write('{} ;{} ;{} \n'.format(municipio, numero_votos, porcentaje).encode('utf8'))
+            out.write('{} ;{} ;{} \n'.format(partido, votos, porcentaje).encode('utf8'))
+            ultimo_partido_leido = partido
+
+    out.close()
 
 
 if __name__ == '__main__':
